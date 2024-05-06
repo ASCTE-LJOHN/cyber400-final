50fb29f70c1138d5cd7a7c60282824e366165c87
ASCTE-LJOHN <logan.johnson@ascte.org>
2024-05-06 11:11:08 -0500

    docker compose install script

---
+++ install-docker-ubuntu.sh
@@ -0,0 +1,13 @@
+# install docker-ce prerequisite
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker vagrant
+# install docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+# must perform a restart of the VM
+sudo reboot now
