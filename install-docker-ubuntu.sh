bb4f0d7fa06eb9b76a9604f28f3ad954ae638b57
ASCTE-LJOHN <logan.johnson@ascte.org>
2024-05-06 14:27:11 -0500

    update docker install

--- install-docker-ubuntu.sh
+++ install-docker-ubuntu.sh
@@ -1,14 +1,23 @@
-# install docker-ce prerequisite
+# https://www.kali.org/docs/containers/installing-docker-on-kali/
 sudo apt update
+sudo apt install -y docker.io
+sudo systemctl enable docker --now
+sudo usermod -aG docker vagrant
+
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt update
+
+# install docker-ce prerequisite
+# sudo apt update
 # sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 # sudo apt update
 # sudo apt install docker-ce -y
-sudo usermod -aG docker vagrant
 # install docker-compose
 # curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 # must perform a restart of the VM
 # sudo reboot now
