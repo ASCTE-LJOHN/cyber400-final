77ba6e49661be9496985123b415fa0afa521397e
ASCTE-LJOHN <logan.johnson@ascte.org>
2024-05-06 14:34:54 -0500

    anther docker update

--- install-docker-ubuntu.sh
+++ install-docker-ubuntu.sh
@@ -7,6 +7,7 @@ sudo usermod -aG docker vagrant
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # install docker-ce prerequisite
 # sudo apt update
@@ -17,7 +18,7 @@ sudo apt update
 # sudo apt install docker-ce -y
 # install docker-compose
 # curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-# curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 # must perform a restart of the VM
 # sudo reboot now
