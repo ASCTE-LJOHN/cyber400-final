d895b4963d8fd65d0ca18ae384ee5f4ca1494c81
ASCTE-LJOHN <logan.johnson@ascte.org>
2024-05-08 09:50:09 -0500

    greenbone install

--- install-greenbone-kali.sh
+++ install-greenbone-kali.sh
@@ -1,6 +1,12 @@
-# Install Greenbone from scratch on Kali Linux
+## Install Greenbone from scratch on Kali Linux
 # source: https://greenbone.github.io/docs/latest/22.4/kali/index.html
+# requires many hours of time to sync feeds
 
 sudo apt update
 sudo apt install gvm -y
-DEBIAN_FRONTEND=noninteractive sudo gvm-setup > gvm_passwd.txt
+sudo gvm-setup > gvm_passwd.txt
+chmod -w gvm_passwd.txt
+sudo gvm-check-setup
+# sudo -E -u _gvm -g _gvm gvmd --user=admin --new-password=???
+
+# sudo reboot now
--- master-vuln1.sh
+++ master-vuln1.sh
@@ -6,7 +6,7 @@ chmod +x *.sh
 DEBIAN_FRONTEND=noninteractive sudo ./install-docker-ubuntu.sh
 
 # Install vulnerable services on Vulnerable Machine #1
-# candidate: https://github.com/vulhub/vulhub/tree/639e32be6c0425b11d7d56594761c5e599a6a9d5/jumpserver/CVE-2023-42820
+# [FAIL] candidate: https://github.com/vulhub/vulhub/tree/639e32be6c0425b11d7d56594761c5e599a6a9d5/jumpserver/CVE-2023-42820
 git clone https://github.com/vulhub/vulhub.git
 cd vulhub/jumpserver/CVE-2023-42820
 sudo docker compose up -d
