eb65c12b2c3552ed576c94086fe906bc1990020d
ASCTE-LJOHN <logan.johnson@ascte.org>
2024-05-10 12:37:31 -0500

    edits to greenbone config

--- install-greenbone-kali.sh
+++ install-greenbone-kali.sh
@@ -9,4 +9,10 @@ chmod -w gvm_passwd.txt
 sudo gvm-check-setup
 # sudo -E -u _gvm -g _gvm gvmd --user=admin --new-password=???
 
+# Allow greenbone to be accessible on all interfaces
+sudo gvm-stop
+# sed -i "stuff" /usr/lib/systemd/system/greenbone-security-assistant.service
+sudo systemctl daemon-reload
+sudo gvm-start
+
 # sudo reboot now
