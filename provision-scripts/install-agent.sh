c946b653a68d945c93503193c48c1bd695efff75
ASCTE-LJOHN <logan.johnson@ascte.org>
2025-05-02 08:25:30 -0500

    elastic-agent files

--- provision-scripts/install-agent.sh
+++ provision-scripts/install-agent.sh
@@ -1,13 +1,15 @@
-## Install elastic agent on ubuntu host
+## Install elastic agent on ubuntu ARM64 host
 # source: https://www.elastic.co/guide/en/fleet/current/install-standalone-elastic-agent.html
+# source: https://www.elastic.co/downloads/elastic-agent
 
 # (1) Download and extract the installation package.
-curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.15.0-linux-x86_64.tar.gz
-tar xzvf elastic-agent-8.15.0-linux-x86_64.tar.gz
+cd ~
+curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-9.0.0-linux-arm64.tar.gz
+tar xzvf elastic-agent-9.0.0-linux-arm64.tar.gz
 
 # (2) Modify settings in the elastic-agent.yml as required.
-cd elastic-agent-8.15.0-linux-x86_64
-cp /host-files/elastic-agent.yml .
+cd elastic-agent-9.0.0-linux-arm64
+cp ~/siem-files/elastic-agent.yml .
 
 # (5) From the agent directory, run the following commands to install Elastic Agent and start it as a service.
 sudo ./elastic-agent install -n # yes then no
