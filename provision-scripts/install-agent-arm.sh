47aaa1f874a4a8ca0fbfc268260b98242ad1e79c
ASCTE-LJOHN <logan.johnson@ascte.org>
2025-05-02 09:11:17 -0500

    add x86 elastic-agent script

---
+++ provision-scripts/install-agent-arm.sh
@@ -0,0 +1,19 @@
+## Install elastic agent on ubuntu ARM64 host
+# source: https://www.elastic.co/guide/en/fleet/current/install-standalone-elastic-agent.html
+# source: https://www.elastic.co/downloads/elastic-agent
+
+# (1) Download and extract the installation package.
+curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-9.0.0-linux-arm64.tar.gz
+tar xzvf elastic-agent-9.0.0-linux-arm64.tar.gz
+
+# (2) Modify settings in the elastic-agent.yml as required.
+cd elastic-agent-9.0.0-linux-arm64
+cp ../elastic-agent.yml .
+
+# (5) From the agent directory, run the following commands to install Elastic Agent and start it as a service.
+sudo ./elastic-agent install -n # yes then no
+
+## uninstall instructions
+# sudo elastic-agent uninstall
+# sudo rm -rf /opt/Elastic/Agent
+# sudo rm -rf /etc/elastic-agent
--- provision-scripts/install-agent-x86.sh
+++ provision-scripts/install-agent-x86.sh
@@ -1,18 +1,16 @@
-## Install elastic agent on ubuntu ARM64 host
+## Install elastic agent on ubuntu host
 # source: https://www.elastic.co/guide/en/fleet/current/install-standalone-elastic-agent.html
-# source: https://www.elastic.co/downloads/elastic-agent
 
 # (1) Download and extract the installation package.
-curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-9.0.0-linux-arm64.tar.gz
-tar xzvf elastic-agent-9.0.0-linux-arm64.tar.gz
+curl -L -O https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-8.15.0-linux-x86_64.tar.gz
+tar xzvf elastic-agent-8.15.0-linux-x86_64.tar.gz
 
 # (2) Modify settings in the elastic-agent.yml as required.
-cd elastic-agent-9.0.0-linux-arm64
+cd elastic-agent-8.15.0-linux-x86_64
 cp ../elastic-agent.yml .
 
 # (5) From the agent directory, run the following commands to install Elastic Agent and start it as a service.
 sudo ./elastic-agent install -n # yes then no
-
 
 ## uninstall instructions
 # sudo elastic-agent uninstall
